d04402c7eeeb8f9ccc6f5dc2ef25ab165a485504
Igor Shapiro <igor.shapiro@lemonade.com>
2018-10-26 21:11:03 +0300

    Crash on eslint errors. Add an option to ignore eslint errors

--- initializers/eslint.js
+++ initializers/eslint.js
@@ -8,15 +8,19 @@ module.exports = async function(app, log, config) {
   require('eslint-plugin-promise')
   require('eslint-config-standard')
 
-  let eslintIgnoreFile = await app.glob('.eslintignore', { dirs: ['app', 'quadro'] })
-  let eslintrcFile = await app.glob('.eslintrc', { dirs: ['app', 'quadro'] })
+  const eslintIgnoreFile = await app.glob('.eslintignore', { dirs: ['app', 'quadro'] })
+  const eslintrcFile = await app.glob('.eslintrc', { dirs: ['app', 'quadro'] })
 
-  let CLIEngine = require('eslint').CLIEngine
-  let cli = new CLIEngine({
+  const CLIEngine = require('eslint').CLIEngine
+  const cli = new CLIEngine({
     ignorePath: eslintIgnoreFile[0],
     configFile: eslintrcFile[0]
   })
-  let report = cli.executeOnFiles([app.appDir])
-  let formatter = cli.getFormatter('compact')
-  console.log(formatter(report.results))
+  const report = cli.executeOnFiles([app.appDir])
+  const formatter = cli.getFormatter('compact')
+  const { results, errorCount } = report
+  console.log(formatter(results))
+
+  const ignoreErrors = config.get('quadro.test.eslint.ignore', false)
+  if (errorCount > 0 && !ignoreErrors) process.exit(1)
 }
